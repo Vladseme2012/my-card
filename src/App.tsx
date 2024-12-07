3d930ac4a33c8ea7f9e5947f59e9b3c9db5b9578
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-07 20:02:28 +0300

    added styled component Box

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css';
 import styled from 'styled-components';
 
+type StyledBoxPropsType = {
+    width: string;
+    border?: string;
+};
 
 function App() {
     return (
         <Card>
+            <Box width={'280px'} border={'15px'}>
+                
+            </Box>
             <Title>Welcome to IT-INCUBATOR</Title>
         </Card>
     );
@@ -13,15 +20,23 @@ function App() {
 export default App;
 
 const Card = styled.div`
-width: 100%;
-max-width: 300px;
-padding: 10px;
-border-radius: 15px;
-display: flex;
-flex-direction: column;
-align-content: flex-start;
-background-color: #FFFFFF;
-`
+    width: 100%;
+    max-width: 300px;
+    padding: 10px;
+    border-radius: 15px;
+    display: flex;
+    flex-direction: column;
+    align-content: flex-start;
+    background-color: #ffffff;
+`;
+
+const Box = styled.div<StyledBoxPropsType>`
+    max-width: ${(props) => props.width};
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    border-radius: ${(props) => props.border};
+`;
 
 const Title = styled.h1`
     font-size: 1.5em;

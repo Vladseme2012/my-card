5fbc4ab96936283a12ce421d1c70d16f74ed9cda
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-07 20:17:44 +0300

    added type StyledBoxPropsType for styled component Box

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import './App.css';
 import styled from 'styled-components';
 
 type StyledBoxPropsType = {
-    width: string;
-    border?: string;
-};
+  width: string;
+  border?: string;
+}
+
 
 function App() {
     return (
         <Card>
-            <Box width={'280px'} border={'15px'}>
-            <img src="./assets/images/photo-of-desert.jpg" alt="desert" />
-            </Box>
+          <Box width={'280px'} border={'15px'}>
+
+          </Box>
             <Title>Welcome to IT-INCUBATOR</Title>
         </Card>
     );
@@ -20,22 +21,22 @@ function App() {
 export default App;
 
 const Card = styled.div`
-    width: 100%;
-    max-width: 300px;
-    padding: 10px;
-    border-radius: 15px;
-    display: flex;
-    flex-direction: column;
-    align-content: flex-start;
-    background-color: #ffffff;
+width: 100%;
+max-width: 300px;
+padding: 10px;
+border-radius: 15px;
+display: flex;
+flex-direction: column;
+align-content: flex-start;
+background-color: #FFFFFF;
 `;
 
 const Box = styled.div<StyledBoxPropsType>`
-    max-width: ${(props) => props.width};
-    display: flex;
-    justify-content: start;
-    align-items: center;
-    border-radius: ${(props) => props.border};
+max-width: ${props => props.width};
+display: flex;
+justify-content: start;
+align-items: center;
+border-radius: ${props => props.border}
 `;
 
 const Title = styled.h1`

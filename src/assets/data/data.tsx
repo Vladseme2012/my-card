bbd809633848cf4ac1871441f7d4908f1ce3b6e1
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-15 20:32:13 +0300

    created array for moke dates for Card

---
+++ src/assets/data/data.tsx
@@ -0,0 +1,18 @@
+import photo from '../images/desert.jpg'
+export const data = [
+    {
+        image: photo,
+        title: 'Headline',
+        text: 'Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.',
+    },
+    {
+        image: photo,
+        title: 'Headline',
+        text: 'Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.',
+    },
+    {
+      image: photo,
+      title: 'Headline',
+      text: 'Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.',
+  },
+];

7338450d7f758a6830061acf76611303b00dd024
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-15 20:37:31 +0300

    fixed component App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,115 +1,32 @@
 import './App.css';
+import { FlexWrapper } from './components/flexWrapper/FlexWrapper';
+import { Card } from './components/card/Card';
+import { data } from './assets/data/data';
 import styled from 'styled-components';
-import { myTheme } from './styles/Theme.styled';
-import photo from './assets/images/desert.jpg';
-
-type StyledBoxPropsType = {
-    width: string;
-    border?: string;
-    margin?: string;
-    content?: string;
-};
-
-type StyledImagePropsType = {
-    src: string;
-    alt: string;
-};
-
-type StyledButtonPropsType = {
-    color: string;
-    background: string;
-};
 
 function App() {
     return (
-        <Card>
-            <Box width={'280px'} border={'15px'} margin={'20px'}>
-                <Image src={photo} alt="desert" />
-            </Box>
-            <Box width={'70px'}>
-                <Title>Headline</Title>
-            </Box>
-            <Box width={'260px'} margin={'10px'}>
-                <Paragraph>
-                    Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit
-                    molestie ornare in venen.
-                </Paragraph>
-            </Box>
-            <Box width={'192px'} margin={'20px'} content={'space-between'}>
-                <Button
-                    color={myTheme.colors.white}
-                    background={myTheme.colors.blue}
-                >
-                    See more
-                </Button>
-                <Button
-                    color={myTheme.colors.blue}
-                    background={myTheme.colors.white}
-                >
-                    Save
-                </Button>
-            </Box>
-        </Card>
+        <StyledSection>
+            <FlexWrapper>
+                {data.map((el) => (
+                    <Card
+                        src={el.image}
+                        title={el.title}
+                        text={el.text}
+                        key={el.title}
+                    />
+                ))}
+            </FlexWrapper>
+        </StyledSection>
     );
 }
 
-export default App;
-
-const Card = styled.div`
-    width: 100%;
-    max-width: 300px;
-    padding: 10px;
-    border-radius: 15px;
+const StyledSection = styled.section`
+    width: 100vw;
+    height: 100vh;
     display: flex;
-    flex-direction: column;
-    align-content: flex-start;
-    background-color: ${myTheme.colors.white};
-`;
-
-const Box = styled.div<StyledBoxPropsType>`
-    max-width: ${(props) => props.width};
-    display: flex;
-    justify-content: ${(props) => props.content || 'start'};
+    justify-content: center;
     align-items: center;
-    border-radius: ${(props) => props.border};
-    margin-bottom: ${(props) => props.margin};
 `;
 
-const Image = styled.img<StyledImagePropsType>`
-width: 280px;
-height: 170px;
-object-fit: cover;
-    src: ${(props) => props.src};
-    alt: ${(props) => props.alt};
-`;
-
-const Title = styled.h2`
-    font-size: 16px;
-    font-wight: bold;
-    text-align: center;
-    color: ${myTheme.colors.black};
-`;
-
-const Paragraph = styled.p`
-    font-size: 12px;
-    font-wight: medium;
-    text-align: left;
-    color: ${myTheme.colors.gray};
-`;
-
-const Button = styled.button<StyledButtonPropsType>`
-    width: 100%;
-    max-width: 86px;
-    padding: 5px;
-    border-radius: 5px;
-    border: 2px solid ${myTheme.colors.blue};
-    text-align: center;
-    font-size: 10px;
-    font-wight: bold;
-    color: ${(props) => props.color};
-    background-color: ${(props) => props.background};
-
-    &: hover {
-        cursor: pointer;
-    }
-`;
+export default App;

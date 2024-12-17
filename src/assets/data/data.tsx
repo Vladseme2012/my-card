bead4c380d52d6f7847fe68a0edeff265dc6d09c
vladseme2012 <vlad.seme2012@yandex.ru>
2024-12-17 20:13:35 +0300

    fixed title and text in array objects

--- src/assets/data/data.tsx
+++ src/assets/data/data.tsx
@@ -7,12 +7,12 @@ export const data = [
     },
     {
         image: photo,
-        title: 'Headline',
-        text: 'Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.',
+        title: 'Headline2',
+        text: 'Sit molestie ornare in venen.',
     },
     {
       image: photo,
-      title: 'Headline',
-      text: 'Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.',
+      title: 'Headline3',
+      text: 'Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.',
   },
 ];
